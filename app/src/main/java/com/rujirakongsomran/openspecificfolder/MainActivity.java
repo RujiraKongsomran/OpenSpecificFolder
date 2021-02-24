99de5ee31383368e80dbd858923e898986bb80f7
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-02-24 15:29:25 +0700

    Enable ViewBinding

--- app/src/main/java/com/rujirakongsomran/openspecificfolder/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/openspecificfolder/MainActivity.java
@@ -3,12 +3,19 @@ package com.rujirakongsomran.openspecificfolder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rujirakongsomran.openspecificfolder.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
